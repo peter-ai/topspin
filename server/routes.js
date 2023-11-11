cef8d7c7f201d309f27b9557a525ad4d68a48574
Peter Akioyamen <peter.akioyamen@gmail.com>
2023-11-10 19:46:10 -0500

    connect to backend and implement test routes

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,44 @@
-const home = (req, res) => {
+const mysql = require('mysql2');
+const config = require('./config');
+
+
+// Creates MySQL connection using database credential provided in config.json
+const connection = mysql.createConnection({
+  host: config.HOST,
+  user: config.USERNAME,
+  password: config.PASSWORD,
+  port: config.PORT,
+  database: config.DATABASE
+});
+connection.connect((err) => err && console.log(err));
+
+
+const home = async (req, res) => {
   res.send("Server homepage");
 };
 
-module.exports = { home };
+// test route to display data
+const test = async (req, res) => {
+  connection.query(
+    `
+    SELECT * FROM player;
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        // If there is an error for some reason, or if the query is empty (this should not be possible)
+        // print the error message and return an empty object instead
+        console.log(err);
+
+        // Be cognizant of the fact we return an empty object {}. For future routes, depending on the
+        // return type you may need to return an empty array [] instead.
+        res.json({});
+      } else {
+        // Here, we return results of the query as an object
+          res.json(data);
+      }
+  });
+}
+
+module.exports = { 
+  home,
+  test, 
+};
