7f182456da308df49d719e5122b17da617373c1a
Peter Akioyamen <peter.akioyamen@gmail.com>
2023-11-14 17:15:20 -0500

    configure route and query for player best surface data

--- server/routes.js
+++ server/routes.js
@@ -82,49 +82,48 @@ const player_surface = async (req, res) => {
 
   // otherwise try execute query
   } else {
-    res.send("Surface route working");
-    // connection.query(
-    //   `
-    //   WITH surface_perf AS (
-    //       WITH win_surface AS (
-    //                         SELECT surface, COUNT(G.winner_id) AS wins
-    //                         FROM game G INNER JOIN tournament T ON G.tourney_id=T.id
-    //                         WHERE G.winner_id=?
-    //                         GROUP BY surface
-    //       ),
-    //       loss_surface AS (
-    //                         SELECT surface, COUNT(G.loser_id) AS losses
-    //                         FROM game G INNER JOIN tournament T ON G.tourney_id=T.id
-    //                         WHERE G.loser_id=?
-    //                         GROUP BY surface
-    //       )
-    //       SELECT w_surface AS surface, IFNULL(wins, 0) AS wins, IFNULL(losses, 0) AS losses,
-    //             IFNULL(wins,0)/(IFNULL(wins,0)+IFNULL(losses,0)) AS win_percentage,
-    //             IFNULL(losses,0)/(IFNULL(wins,0)+IFNULL(losses,0)) AS loss_percentage
-    //       FROM ((SELECT W.surface AS w_surface, wins, losses, L.surface AS L_surface
-    //             FROM win_surface W LEFT JOIN loss_surface L on W.surface=L.surface)
-    //             UNION
-    //             (SELECT W.surface AS w_surface, wins, losses, L.surface AS L_surface
-    //             FROM win_surface W RIGHT JOIN loss_surface L on W.surface=L.surface)) WL
-    //   )
-    //   SELECT *
-    //   FROM surface_perf
-    //   WHERE win_percentage=(SELECT MAX(win_percentage) FROM surface_perf)
-    //     OR loss_percentage=(SELECT MAX(loss_percentage) FROM surface_perf);
-    //   `,
-    //   [player_id],
-    //   (err, data) => {
-    //     // if error or no data was returned (id out of range), send empty json
-    //     if (err || data.length === 0) {
-    //       console.log(err);
-    //       res.json([]);
+    connection.query(
+      `
+      WITH surface_perf AS (
+          WITH win_surface AS (
+                            SELECT surface, COUNT(G.winner_id) AS wins
+                            FROM game G INNER JOIN tournament T ON G.tourney_id=T.id
+                            WHERE G.winner_id=?
+                            GROUP BY surface
+          ),
+          loss_surface AS (
+                            SELECT surface, COUNT(G.loser_id) AS losses
+                            FROM game G INNER JOIN tournament T ON G.tourney_id=T.id
+                            WHERE G.loser_id=?
+                            GROUP BY surface
+          )
+          SELECT w_surface AS surface, IFNULL(wins, 0) AS wins, IFNULL(losses, 0) AS losses,
+                IFNULL(wins,0)/(IFNULL(wins,0)+IFNULL(losses,0)) AS win_percentage,
+                IFNULL(losses,0)/(IFNULL(wins,0)+IFNULL(losses,0)) AS loss_percentage
+          FROM ((SELECT W.surface AS w_surface, wins, losses, L.surface AS L_surface
+                FROM win_surface W LEFT JOIN loss_surface L on W.surface=L.surface)
+                UNION
+                (SELECT W.surface AS w_surface, wins, losses, L.surface AS L_surface
+                FROM win_surface W RIGHT JOIN loss_surface L on W.surface=L.surface)) WL
+      )
+      SELECT *
+      FROM surface_perf
+      WHERE win_percentage=(SELECT MAX(win_percentage) FROM surface_perf)
+        OR loss_percentage=(SELECT MAX(loss_percentage) FROM surface_perf);
+      `,
+      [player_id],
+      (err, data) => {
+        // if error or no data was returned (id out of range), send empty json
+        if (err || data.length === 0) {
+          console.log(err);
+          res.json([]);
         
-    //     // if query successful
-    //     } else {
-    //       res.json(data);
-    //     }
-    //   }
-    // );
+        // if query successful
+        } else {
+          res.json(data);
+        }
+      }
+    );
   }
 }
 
