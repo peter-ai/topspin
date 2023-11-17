f2aced8ceed7563c797b342e98500963aac443e2
Bilal Ali <bilalal495@outlook.com>
2023-11-16 22:13:45 -0500

    Prepend api to all server routes

--- server/index.js
+++ server/index.js
@@ -14,15 +14,14 @@ app.use(cors()); // allow client requests
 app.use(morgan("tiny")); // logger
 
 // routes
-// TODO prepend all routes with api to distinguish client/server routes
-app.get("/", routes.home);
-app.get("/player", routes.player);
-app.get("/player/:id", routes.player_info);
-app.get("/player/:id/stats", routes.player_stats);
-app.get("/player/:id/surface", routes.player_surface);
-app.get("/player/:id/matches", routes.player_matches);
-app.get("/tournament/:tourney_id/:match_num", routes.single_match);
-app.get("/compare/:player1/:player2", routes.compare);
+app.get("/api", routes.home);
+app.get("/api/player", routes.player);
+app.get("/api/player/:id", routes.player_info);
+app.get("/api/player/:id/stats", routes.player_stats);
+app.get("/api/player/:id/surface", routes.player_surface);
+app.get("/api/player/:id/matches", routes.player_matches);
+app.get("/api/tournament/:tourney_id/:match_num", routes.single_match);
+app.get("/api/compare/:player1/:player2", routes.compare);
 
 // listen for requests
 app.listen(`${SERVER_PORT}`, () => {
