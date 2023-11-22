67557e0207121f6952264f78353e6c08de81ba40
Peter Akioyamen <peter.akioyamen@gmail.com>
2023-11-22 17:52:51 -0500

    implement SearchBar component

--- client/src/pages/PlayerPage.jsx
+++ client/src/pages/PlayerPage.jsx
@@ -1,69 +1,74 @@
 import { useEffect, useState } from 'react';
 
+import SearchBar from "../component/SearchBar";
+
 // declare server port and host for requests
 const SERVER_PORT = import.meta.env.VITE_SERVER_PORT;
 const SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
 
 export default function PlayerPage() {
-    const [players, setPlayers] = useState([]); // variable for list of players
-    const [searchInput, setSearchInput] = useState(''); // variable tracking state of search bar
-    const [leagueInput, setLeagueInput] = useState('Both');
+  const [players, setPlayers] = useState([]); // variable for list of players
+  const [searchInput, setSearchInput] = useState(''); // variable tracking state of search bar
+  const [leagueInput, setLeagueInput] = useState('Both');
+  
+  // use effect
+  useEffect(() => {
+    fetch(`http://${SERVER_HOST}:${SERVER_PORT}/api/player`) // send get request to /player route on server
+    .then((res) => res.json()) // convert response to json
+    .then((resJson) => setPlayers(resJson)) // set players 
+    .catch((err) => console.log(err)); // catch and log errors
+  }, []); // [] empty listener, so only run effect on load of page
+
+  // handles change of league dropdown
+  const handleChange = (e) => {
+    e.preventDefault();
+    setLeagueInput(e.target.value);
+  }
+  
+  // function to set state of search bar
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value); // set variable to the current value of search bar
+  };
     
-    // use effect
-    useEffect(() => {
-        fetch(`http://${SERVER_HOST}:${SERVER_PORT}/api/player`) // send get request to /player route on server
-        .then((res) => res.json()) // convert response to json
-        .then((resJson) => setPlayers(resJson)) // set players 
-        .catch((err) => console.log(err)); // catch and log errors
-    }, []); // [] empty listener, so only run effect on load of page
+  // function to output players based on matching search results for player names and specified league
+  const searchPlayers = () => {
+    return players.filter((player) => {
+      return (
+        (leagueInput === 'Both' || player.league === leagueInput.toLowerCase()) && // if league is Both or player league matches input league
+        player.name.toLowerCase().includes(searchInput.toLowerCase()) // if player name includes the search string, empty str matches true for all
+      )
+    }).map((player) => (
+      // construct list of players to display
+      <li key={player.id}>
+        <a href={'/player/'+player.id}>{player.name}</a>: {player.ioc} | {player.league}
+      </li>
+    ));
+  };
 
-    // handles change of league dropdown
-    const handleChange = (e) => {
-        e.preventDefault();
-        setLeagueInput(e.target.value);
-    }
-    
-    // function to set state of search bar
-    const handleSearch = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value); // set variable to the current value of search bar
-    };
-      
-    // function to output players based on matching search results for player names and specified league
-    const searchPlayers = () => {
-        return players.filter((player) => {
-            return (
-                (leagueInput === 'Both' || player.league === leagueInput.toLowerCase()) && // if league is Both or player league matches input league
-                player.name.toLowerCase().includes(searchInput.toLowerCase()) // if player name includes the search string, empty str matches true for all
-            )
-        }).map((player) => (
-            // construct list of players to display
-            <li key={player.id}>
-                <a href={'/player/'+player.id}>{player.name}</a>: {player.ioc} | {player.league}
-            </li>
-        ));
-    };
+  return (
+    <>
+      <h1>Player Directory</h1>
+      <SearchBar defaultValue={searchInput} placeholder='Name' handleSearch={handleSearch}/>
 
-    return (
-        <>
-            <h1>Players</h1>
-            <label htmlFor='league'>League: </label>
 
-            <select name='league' id='league' value={leagueInput} onChange={handleChange}>
-                <option value='Both'>Both</option>
-                <option value='WTA'>Women's (WTA)</option>
-                <option value='ATP'>Men's (ATP)</option>
-            </select>
+      <label htmlFor='league'>League: </label>
 
-            <input
-                type="search"
-                placeholder="Search by name"
-                onChange={handleSearch}
-                value={searchInput} 
-            />            
-            <ul>
-                {searchPlayers()}
-            </ul>
-        </>
-    );
+      <select name='league' id='league' value={leagueInput} onChange={handleChange}>
+        <option value='Both'>Both</option>
+        <option value='WTA'>Women's (WTA)</option>
+        <option value='ATP'>Men's (ATP)</option>
+      </select>
+
+      {/* <input
+          type="search"
+          placeholder="Search by name"
+          onChange={handleSearch}
+          value={searchInput} 
+      />             */}
+      <ul>
+        {searchPlayers()}
+      </ul>
+    </>
+  );
 }
