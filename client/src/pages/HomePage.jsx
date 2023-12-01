488b48760f99dec54dc12f224571009a44656968
Bilal Ali <bilalal495@outlook.com>
2023-11-30 22:32:54 -0500

    Add gif of tennis player to home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import Typography from "@mui/material/Typography";
-
+import { Box, Grid, Typography } from "@mui/material";
 // declare server port and host for requests
 const SERVER_PORT = import.meta.env.VITE_SERVER_PORT;
 const SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
@@ -15,7 +14,28 @@ export default function HomePage() {
 
   return (
     <>
-      <Typography variant="h1">TopSpin</Typography>
+      <Grid
+        container
+        direction="row"
+        maxWidth="xl"
+        alignItems="center"
+        marginY="100px"
+        width="100%"
+        marginX="auto"
+      >
+        <Grid item xs={6} textAlign="center"></Grid>
+        <Grid item xs={6}>
+          <Box
+            component="img"
+            sx={{
+              maxWidth: "sm",
+              borderRadius: "50%",
+            }}
+            src="/src/assets/imgs/homepage.gif"
+            title="gif of tennis player serving a ball"
+          ></Box>
+        </Grid>
+      </Grid>
     </>
   );
 }
