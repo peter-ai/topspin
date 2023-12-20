3056816a75926860cbec1e96e5c652eaa0da0cbc
Peter Akioyamen <peter.akioyamen@gmail.com>
2023-12-19 21:12:04 -0500

    complete ml model and flask updates

--- src/ml/topspin_ml.py
+++ src/ml/topspin_ml.py
@@ -1,35 +1,38 @@
-
+# import packages
 import json
-from flask import Flask
-from flask_cors import CORS, cross_origin
 import joblib
 import numpy as np
+from flask import Flask
+from flask_cors import CORS, cross_origin
 
-MODEL_DIMENSION = 18
+# define expected dimension of input
+MODEL_DIMENSION = 18 
+
+# load classification model
 with open('GBM_classifier.joblib', 'rb') as f:
     clf = joblib.load(f)
 
+# allow CORS and start server
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/")
-@cross_origin()
-def home():
-    return "<p>Tennis Match Predictor</p>"
-
+# define route for predictions
 @app.route("/predict/<features>", methods=['GET'])
 @cross_origin()
 def predict(features):
     try:
-        print(features)
-        v = np.array([float(feature) if feature != 'None' else np.NaN for feature in features.split(',') ])
+        # parse input vector from GET params
+        v = np.array([float(feature) if feature != 'None' else np.NaN for feature in features.split(',')])
     except ValueError as e:
         v = None
 
+    # check if dimension is as expected
     if v is None or len(v) != MODEL_DIMENSION:
         return json.dumps({'prediction': None})
     
+    # predict match outcome
     pred = clf.predict(v[None,:])[0]
     
+    # return result
     return json.dumps({'prediction': pred})
